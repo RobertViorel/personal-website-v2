be911904b8ce7829326679d422fb00874c213de5
Robert051998 <96495255+Robert051998@users.noreply.github.com>
2024-09-10 15:14:43 +0200

    next head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import { Navbar } from "./components/Navbar";
 import MouseMovement from "./components/MouseMovement";
 import { CookieConsent } from "./components/CookieConsent";
@@ -51,6 +52,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
       <body className={`bg-primary ${inter.className} flex flex-col min-h-screen`}>
         <Navbar />
         <MouseMovement />
